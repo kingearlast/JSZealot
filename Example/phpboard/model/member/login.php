3ed27e1556c0d2a16f77be3e75355e236a4b17fd
kingearlast <kingearlast@gmail.com>
2012-02-10 14:23:19 +0900

    add yg pt_mytarget

--- Example/phpboard/model/member/login.php
+++ Example/phpboard/model/member/login.php
@@ -2,29 +2,23 @@
 	session_start();
 	@extract($_POST);
     include_once '../../common/connect_db.php';
+	include_once '../../common/util.php';
 	
 	if($id == "" || $pwd == "") {
-		echo "<script>alert('입력 에러')</script>";
-		die;
+		page_alert_and_redirect('입력 에러', '/phpboard');
 	}
 	
 	$sql = "select count(*) from mb_inf where mb_inf_id = '$id' and mb_inf_pwd = '$pwd'";
 	$result = mysql_query($sql, $connect);
 	$resultSet = mysql_fetch_row($result);
-	$row_num = $resultSet[0];
+	$rowNum = $resultSet[0];
 	
 	echo $resultSet[0];
 	
-	if($row_num > 0) {
+	if($rowNum > 0) {
 		$_SESSION['loginID'] = $id;
-		echo "<script>
-			      alert('로그인 성공');
-			      location.replace('/phpboard');
-			  </script>";
+		page_redirect('/phpboard');
 	}else {
-		echo "<script>
-			      alert('아이디나 패스워드가 틀립니다.');
-			      location.replace('/phpboard');
-			  </script>";
+		page_alert_and_redirect('아이디나 패스워드가 틀립니다.', '/phpboard');
 	}
 ?>
